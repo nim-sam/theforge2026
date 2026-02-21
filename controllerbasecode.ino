1fbf8244f704aeee52d06d95787c184cfd5c1814
stella-vw <stella.vandewater@mail.mcgill.ca>
2026-02-21 18:29:44 -0500

    shoot function

--- controllerbasecode.ino
+++ controllerbasecode.ino
@@ -3,15 +3,23 @@
 // ports:
 // nav motors: ENA-A0, IN1-2, IN2-3 ; motor2: ENB-A1, IN3-4, IN4-5
 // shoot motor: ENA-A2, IN1-6, IN2-7
+int shootMotor1 = 6;
+int shootMotor2 = 7;
 
 Controller controller("LeBronClanks", "lebron1234");
 
 void setup() {
   Serial.begin(115200);
 
+  //nav motor configs
   controller.configureL298N(A0, 2, 3, A1, 4, 5); //ENA, IN1, IN2, ENB, IN3, IN4
   controller.setMotorMinPWM(120);
   controller.setFailsafeTimeoutMs(1000);
+
+  //shoot motor configs
+  pinMode(shootMotor1, OUTPUT);
+  pinMode(shootMotor2, OUTPUT);
+  pinMode(A2, OUTPUT);
 
   //buttons
   controller.registerButton("SHOOT", shoot);
@@ -26,10 +34,15 @@ void loop() {
 }
 
 //button functions
-
 void shoot() {
   // add shoot function here
+  analogWrite(A2, 100); //speed
+  digitalWrite(6, HIGH); // spin motors
+  digitalWrite(7, LOW);
+  delay(3000);
+
   Serial.println("shoot button pressed");
+
 }
 
 void angleUp() {
