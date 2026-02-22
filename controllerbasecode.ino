ea3b82254cd535b3a3323e4e6323862bb6086245
Sarah Houria Abdelaoui <sarahhouria.abdelaoui@gmail.com>
2026-02-21 19:40:03 -0500

    setting up the servo motors

--- controllerbasecode.ino
+++ controllerbasecode.ino
@@ -1,11 +1,15 @@
 #include <Controller.h>
 #include <Servo.h>
 
+Servo tilt; // to tilt the system
+Servo laserPointer; // for laser pointer
+
 // ports:
 // nav motors: ENA-A0, IN1-2, IN2-3 ; motor2: ENB-A1, IN3-4, IN4-5
 // shoot motor: ENA-A2, IN1-6, IN2-7
 int shootMotor1 = 6;
 int shootMotor2 = 7;
+int tilt = A0;
 
 Controller controller("LeBronClanks", "lebron1234");
 
@@ -28,6 +32,9 @@ void setup() {
   controller.registerButton("ANGLE DECREASE", angleDown);
 
   controller.beginAP(true);
+
+  // tilt servo motor
+  pinMode(A0, OUTPUT);
 }
 
 void loop() {
